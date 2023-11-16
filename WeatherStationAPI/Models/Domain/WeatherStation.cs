94bb0fa9ab8fc824314edaa1f1e5438152686bf6
MarusicAnte <antemarusic1950@gmail.com>
2023-11-16 23:28:39 +0100

    Initial commit

---
+++ WeatherStationAPI/Models/Domain/WeatherStation.cs
@@ -0,0 +1,36 @@
+﻿/*
+ **********************************
+ * Author: Ante Marusic
+ * Project Task: Homework 3 - Weather Station API
+ **********************************
+ * Description:
+ *  
+ *  This file contains model which defines Weather Station class and it's properties.
+ *
+ **********************************
+ */
+
+
+namespace WeatherStationAPI.Models.Domain
+{
+    public class WeatherStation
+    {
+        // Unique ID
+        public int Id { get; set; }
+
+        // Weather Station name
+        public string Name { get; set; }
+
+        //  Weather Station location
+        public string Location{ get; set; }
+
+        // Value of temperature
+        public double Temperature { get; set; }
+
+        // Value of humidity
+        public double Humidity { get; set; }
+
+        // Value of wind speed
+        public double WindSpeed { get; set; }
+    }
+}
